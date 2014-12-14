c443145390f3490b8a156b90d7b3a26d99af723a
Chia <pretend15@gmail.com>
2014-12-14 14:13:05 -0800

    apply bootstrap template

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,11 +1,26 @@
 var bluebird = require('bluebird');
 var services = require('../services/services');
+var fs = require('fs');
+var path = require('path');
 
 module.exports = {
   dailybible: {
+
+    /*fetch today's bilbe chapter*/
     get: function (req, res) {
-      services.fetchPage(function(result){
-        res.status(200).send(result);
+      var today = new Date();
+      var year = today.getFullYear(), month = today.getMonth()+1, 
+      date = (today.getDate()<10)? '0'+today.getDate(): today.getDate();
+      var url = 'http://www.ccreadbible.org/Members/Bona/ccreadbible/maindata/'+
+             year+'/'+month+'/'+year+'-'+month+'-'+date+'.html';
+     
+      services.fetchPage(url, function(result){
+        var file = path.join(__dirname,'../archive/bible.html');
+        fs.writeFile(file, result, function(err){
+          if(err) console.log(err);
+        });
+        
+        res.status(200).json({content: result});
       });
     }
   }
--- server/services/services.js
+++ server/services/services.js
@@ -1,14 +1,10 @@
 /*utility functions can be used across modules*/
-var fs = require('fs');
-var path = require('path');
 var request = require('request');
 
 module.exports = {
-  /*fetch today's bilbe chapter*/
-  fetchPage: function(callback){
-    request('http://www.ccreadbible.org/Members/Bona/ccreadbible/maindata/2014/12/2014-12-08.html', function(error,response, body){
+  fetchPage: function(url, callback){
+    request(url, function(error,response, body){
       if (!error && response.statusCode == 200) {
-        console.log(body) // Print the google web page.
         callback(body);
       }
     });
